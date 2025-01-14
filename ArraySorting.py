b1c23d586e40c0505b2759b3104b2cb1a26ac4d8
unknown <davide.sandrin.24@stud.itsaltoadriatico.edu.it>
2025-01-14 10:04:13 +0100

    try 1 heap sort

--- ArraySorting.py
+++ ArraySorting.py
@@ -1,5 +1,30 @@
-#array sorting usando il .sort
+#array sorting usando il metodo heap sort
+
+def heapify(array, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and array[i] < array[l]:
+        largest = l
+
+    if r < n and array[largest] < array[r]:
+        largest = r
+
+    if largest != i:
+        array[i], array[largest] = array[largest], array[i]
+        heapify(array, n, largest)
+
+def heapSort(array):
+    n = len(array)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(array, n, i)
+
+    for i in range(n - 1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        heapify(array, i, 0)
 
 array = (8586,8658,468,465,8496)
-
-print(sorted(array))
+heapSort(array)
+print("Sorted array is", array)
